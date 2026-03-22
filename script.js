f21a407cd54d274bf34ecd3105fc6c46275a0826
BossHi2 <157933871+BossHi2@users.noreply.github.com>
2026-03-22 12:02:58 -0500

    worked on page 2
    made the timeline of the world leaders during world war 2 (their deaths) and fixed the fade-in animation.

--- script.js
+++ script.js
@@ -4,17 +4,36 @@ function changePages(from, to){
     const fromElem = document.getElementById(from)
     const toElem = document.getElementById(to)
     
-    fromElem.classList.add('page-closed')
+    fromElem.classList.remove("page-opened")
     
     
 
     setTimeout( () =>{
         fromElem.style.display = "none"
-        //fromElem.classList.remove('animate-out')
         toElem.style.display = 'block'
-        toElem.classList.remove('page-closed')
+
+        void toElem.offsetHeight;
+
+        toElem.classList.add('page-opened')
         
-        //toElem.classList.add('page-opened')
         
     }, 1000)
+
+    if(toElem.id == "page2"){
+        makeTimeline()
+    }
 }
+
+
+function makeTimeline(){
+    var events = document.getElementsByClassName("event-wrapper")
+
+    for(var i = 0; i<events.length; i++){
+        var date = parseInt(events[i].getElementsByClassName("date")[0].innerHTML)
+        
+        events[i].style.left = ((document.body.clientWidth - 50)/(2026-1945)) * (date-1945) + "px"
+        console.log(document.body.clientWidth)
+    }
+}
+
+makeTimeline()
